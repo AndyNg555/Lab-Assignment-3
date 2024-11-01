8ab4e412c970e12fc56c07ee4ab9e39d3ba028a2
AndyNg555 <147752774+AndyNg555@users.noreply.github.com>
2024-10-31 23:32:42 -0600

    Add files via upload

---
+++ Lab Assignment 3/ToDoList.jsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function ToDoList({ tasks }) {
+  return (
+    <View>
+      {tasks.map((task, index) => (
+        <View key={index} style={styles.task}>
+          <Text style={styles.taskText}>{task}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
+
+export default ToDoList;
